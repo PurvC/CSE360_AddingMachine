ccdaf1d9911b339016006cfd96d52ffc9e79b194
Purv Chauhan <purvchauhan@gmail.com>
2019-10-07 20:43:36 -0700

    Added AddingMachine class and javadoc comments.

---
+++ CSE360_Assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,59 @@
+package cse360assign2;
+
+/**
+ * This class implements add, subtract and total.
+ * @author purvchauhan
+ * @version 1.0
+ */
+public class AddingMachine {
+
+	//local variables
+	private int total;
+	
+	/**
+	 * This constructs an object of type adding machine and sets total = 0.
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This method returns the total.
+	 * @return total of type int
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This adds a number of type int to total.
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This subtracts a number of type int from total.
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	/**
+	 * This keeps a history of all the operations performed
+	 * and returns a string.
+	 * @return history of operations performed as a string
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * This clears all history and reset the total to 0.
+	 */
+	public void clear() {
+	
+	}
+}
